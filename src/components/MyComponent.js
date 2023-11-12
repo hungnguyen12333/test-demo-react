e782467e254dae05e99f1397c67feff4c86fdd22
hungn12333 <hungn12333@gmail.com>
2023-11-12 16:39:17 +0700

    tesst1

---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render(){
+        return (
+            <div>fisrt component
+                {Math.random()}
+            </div>
+        );
+    }
+}
+export default MyComponent;
