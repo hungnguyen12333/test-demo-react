25852a880eef028a45fa519ac397d6a12a5d3b66
hung nguyen <hungn12333@gmail.com>
2023-11-21 23:59:56 +0700

    update

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    state= {
+        address : "25 ao doi",
+        x :"y"
+    }
+    demo(){
+        console.log("xin chao");
+    }
     render(){
         return (
             <div>
-                fisrt component
+                fisrt component ádsad {this.state.address}
                 {Math.random()}
+
+                <button onClick={this.demo}>hello</button>
             </div>
         );
     }
